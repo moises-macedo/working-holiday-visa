9ab75216ce0c947847423b9e297165d4618b66b6
Moisés Macedo <moises.castro@concentrix.com>
2024-09-26 21:24:57 -0300

    fix: correção de path

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ["react", "react-dom", "react-router-dom"],
+          react: ["react"],
         },
       },
     },
